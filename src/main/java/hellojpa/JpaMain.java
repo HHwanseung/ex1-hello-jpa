7b55d00d81fcb7fcf7a56792fc56f03115d68969
seunghwanLee <dkqlch@naver.com>
2022-05-03 18:56:20 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,15 +16,13 @@ public class JpaMain {
 
         try {
 
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            em.clear();
+            em.persist(member);
 
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("==================");
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
