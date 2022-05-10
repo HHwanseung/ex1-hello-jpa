f32359ce9f65aeb15d6500cbc09578bec3ec0d20
seunghwanLee <dkqlch@naver.com>
2022-05-10 19:08:33 +0900

    양방향 연관관계와 연관관계의 주인2

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -7,14 +7,18 @@ import java.util.List;
 @Entity
 public class Team {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
+
+    public void addMember(Member member) {
+        member.setTeam(this);
+        members.add(member);
+    }
 
     public Long getId() {
         return id;
@@ -38,5 +42,14 @@ public class Team {
 
     public void setMembers(List<Member> members) {
         this.members = members;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", members=" + members +
+                '}';
     }
 }
